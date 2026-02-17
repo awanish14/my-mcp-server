b2992a2168280594569ba0854d8131a5a50143a1
awanish14 <awanish14@gmail.com>
2026-02-18 00:54:48 +0530

    Update server.py

--- server.py
+++ server.py
@@ -48,12 +48,4 @@ def get_processing_history() -> str:
 # --- SERVER RUNNER ---
 
 if __name__ == "__main__":
-    import os
-
-    port = int(os.environ.get("PORT", 8000))
-
-    mcp.run(
-        transport="http",
-        host="0.0.0.0",
-        port=port
-    )
+    mcp.run(transport="http")
